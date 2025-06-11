456b689b45a546fbe8e213469c427d2298319b6b
Arkhipenka <europeoplebelarus@gmail.com>
2025-06-11 21:31:53 +0200

    feat: page

--- src/pages/shoppingCartPage/shoppingCartPage.ts
+++ src/pages/shoppingCartPage/shoppingCartPage.ts
@@ -1,17 +1,15 @@
 export class ShoppingCartPage {
-    constructor() {
-        this.render();
-    }
-    private render(): void {
-        const container = document.createElement('div');
-        container.className = 'shopping-cart-container';
+  constructor() {
+    this.render();
+  }
+  private render(): void {
+    const container = document.createElement('div');
+    container.className = 'shopping-cart-container';
 
-        const title = document.createElement('h1');
-        title.textContent = 'Корзина покупок';
-        container.appendChild(title);
+    const title = document.createElement('h1');
+    title.textContent = 'Корзина покупок';
+    container.appendChild(title);
 
-
-        document.body.appendChild(container);
-    }
-
+    document.body.appendChild(container);
+  }
 }
